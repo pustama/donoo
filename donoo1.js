dd8d8a4f15ebb7922a64e49ebf837d9990d82f90
pustama <pustama@users.noreply.github.com>
2017-12-29 11:17:27 +0700

    Add files via upload

---
+++ donoo1.js
@@ -0,0 +1,33 @@
+    $.each($(".post-share[data-id]"), function(a, e) {
+        var l = $(e).parent().find("#postshares").addClass("share-load"),
+            i = new Firebase("https://sukses-3bdcb.firebaseio.com/pages/id/" + $(e).attr("data-id"));
+        i.once("value", function(a) {
+            var n = a.val(),
+                t = !1;
+            null == n && (n = {}, n.value = 0, n.url = window.location.href, n.id = $(e).attr("data-id"), t = !0), l.removeClass("share-load").text(n.value), n.value++, "/" != window.location.pathname && (t ? i.set(n) : i.child("value").set(n.value))
+        })
+    });
+
+
+    function loadscript(filename) {
+      var scr=document.createElement('script');
+      scr.setAttribute("type","text/javascript");
+      scr.setAttribute("src",filename);
+      document.getElementsByTagName("head")[0].appendChild(scr);
+    }
+    var waitElm=null;
+    function urltinyfyprompt_callback(response) {
+      if(waitElm) waitElm.style.display="none";
+      var txt="Short URL";
+      if(response.title && response.title!="") txt+=' for "'+response.title+'"';
+      else if(response.longurl && response.longurl!="") txt+=" for "+response.longurl;
+      if(response.tinyurl && response.tinyurl!="")
+        prompt(txt+":", response.tinyurl);
+      else
+        alert("Could not get short URL, try again later." + ((response.error && response.error != "") ? ("nError: " + response.error) : ""));
+    }
+    function shortenUrl(elm, long_url, service) {
+      waitElm=elm.parentNode.nextSibling;
+      if(waitElm) waitElm.style.display="inline";
+      loadscript("https://urltinyfy.appspot.com/"+service+"?unify=1&url="+encodeURIComponent(long_url) + "&callback=urltinyfyprompt_callback");
+    }
